2ae0dd7eb49b6d49a513eb314a66a3fe3a21ba44
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-12-08 01:31:52 +0700

    handling gradle error &added  getCost function

--- lib/services/masterdataservice.dart
+++ lib/services/masterdataservice.dart
@@ -1,6 +1,7 @@
 part of 'services.dart';
 
 class MasterDataService {
+
   static Future<List<Province>> getProvince() async {
     var response = await http.get(Uri.https(Const.baseUrl, "/starter/province"),
         headers: <String, String>{
@@ -13,8 +14,8 @@ class MasterDataService {
 
     if (response.statusCode == 200) {
       result = (job['rajaongkir']['results'] as List)
-          .map((e) => Province.fromJson(e))
-          .toList();
+          .map((element) => Province.fromMap(element)).toList();
+
     }
     return result;
   }
@@ -31,9 +32,10 @@ class MasterDataService {
     List<City> result = [];
     if (response.statusCode == 200) {
       result = (job['rajaongkir']['results'] as List)
-          .map((e) => City.fromJson(e))
+          .map((e) => City.fromMap(e))
           .toList();
     }
+
 
     List<City> selectedCities = [];
     for(var c in result){
@@ -41,9 +43,34 @@ class MasterDataService {
         selectedCities.add(c);
       }
     }
-
     return selectedCities;
 
+  }
+
+  static Future<List<Costs>> getCost(var origin, var destination, var weight, var courier) async{
+    var response = await http.post(Uri.https(Const.baseUrl, "/starter/cost"),
+        headers: <String, String>{
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'key': Const.apiKey,
+        },
+        body:<String,dynamic> {
+        'origin': origin.toString(),
+        'destination': destination.toString(),
+        'weight': weight.toString(),
+        'courier': courier.toString(),
+        },);
+
+    var job = json.decode(response.body);
+
+    List<Costs> result = [];
+    print(job['rajaongkir']['results'][0]['costs'] as List);
+    if (response.statusCode == 200) {
+      result = (job['rajaongkir']['results'][0]['costs']  as List)
+          .map((e) => Costs.fromMap(e))
+          .toList();
+    }
+
+    return result;
   }
 
 
